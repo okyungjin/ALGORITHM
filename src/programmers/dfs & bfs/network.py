e0fe1146b1d9010bb1fa9805b084157ce06f647c
okyungjin <okyungjin@gmail.com>
2022-01-24 23:58:00 +0900

    [DFS/BFS] Add boilerplate for 네트워크 in programmers

---
+++ src/programmers/dfs & bfs/network.py
@@ -0,0 +1,11 @@
+def solution(n, computers):
+    answer = 0
+    return answer
+
+
+if __name__ == '__main__':
+    _n = 3
+    _computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+
+    ans = solution(_n, _computers)
+    print(ans)
