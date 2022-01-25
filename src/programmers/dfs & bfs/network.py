c68435751974a9d37f88a2006855ef07ac7eed53
okyungjin <okyungjin@gmail.com>
2022-01-25 23:01:18 +0900

    [DFS/BFS] Solve 네트워크 in programmers

--- src/programmers/dfs & bfs/network.py
+++ src/programmers/dfs & bfs/network.py
@@ -1,11 +1,28 @@
 def solution(n, computers):
+    visited = [False] * n
     answer = 0
+
+    for i in range(n):
+        if not visited[i]:
+            dfs(i, visited, n, computers)
+            answer += 1
+
     return answer
+
+
+def dfs(start, visited, n, computers):
+    visited[start] = True
+
+    for i in range(n):
+        if computers[start][i] == 1 and not visited[i]:
+            visited[i] = True
+            dfs(i, visited, n, computers)
 
 
 if __name__ == '__main__':
     _n = 3
     _computers = [[1, 1, 0], [1, 1, 0], [0, 0, 1]]
+    # _computers = [[1, 1, 0], [1, 1, 1], [0, 1, 1]]
 
     ans = solution(_n, _computers)
     print(ans)
