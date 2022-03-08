52ae0af48672594a078a5a8f13e9919887a55ed7
okyungjin <okyungjin@gmail.com>
2022-03-08 19:30:45 +0900

    [이코테] 최단경로 - 개선된 Dijkstra 구현

---
+++ src/이코테/최단경로/improved_dijkstra.py
@@ -0,0 +1,43 @@
+# 개선된 다익스트라
+from dis import dis
+import heapq
+
+INF = int(1e9)
+
+f = open('src/이코테/최단경로/dijkstra.txt', 'r')
+
+node_cnt, edge_cnt = map(int, f.readline().rstrip().split())
+start = int(f.readline().rstrip())
+
+graph = [[] for _ in range(node_cnt + 1)]
+visited = [False] * (node_cnt + 1)
+distance = [INF] * (node_cnt + 1)
+
+for _ in range(edge_cnt):
+	start_node, end_node, cost = map(int, f.readline().rstrip().split())
+	graph[start_node].append((end_node, cost))
+
+
+def improved_dijkstra(start):
+	q = []
+	heapq.heappush(q, (0, start))
+	while q:
+		dist, now = heapq.heappop(q)
+		if distance[now] < dist:
+			continue
+
+		for i in graph[now]:
+			cost = dist + i[1]
+			if cost < distance[i[0]]:
+				distance[i[0]] = cost
+				heapq.heappush(q, (cost, i[0]))
+
+
+
+improved_dijkstra(start)
+
+for i in range(1, node_cnt + 1):
+	if (distance[i] == INF):
+		print('Infinity')
+	else:
+		print(distance[i])
