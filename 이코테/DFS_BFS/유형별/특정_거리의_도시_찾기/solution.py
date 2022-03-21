ef8717bd0210d63ea4a32839d2244a27af3d6720
okyungjin <okyungjin@gmail.com>
2022-03-21 20:07:17 +0900

    [이코테] DFS/BFS - 특정 거리의 도시 찾기 풀이

--- 이코테/DFS_BFS/유형별/특정_거리의_도시_찾기/solution.py
+++ 이코테/DFS_BFS/유형별/특정_거리의_도시_찾기/solution.py
@@ -1,32 +1,43 @@
 import os
+from collections import deque
+
 
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 f = open(os.path.join(__location__, 'input.txt'), 'r')
 
-# 특정 도시 X로 출발 -> 다익스트라
-# 최단 거리가 K인 모든 도시의 번호 출력
-
-
-
+nTestCase = int(f.readline())
 
 # N M K X
 # A B (from, to)
 
-# bfs로 풀이
-nCity, nEdge, start, k = map(int, f.readline().split())
-graph = [[] for _ in range(nCity + 1)]
+for _ in range(nTestCase):
+    nCity, nEdge, k, start = map(int, f.readline().split())
+    graph = [[] for _ in range(nCity + 1)]
 
-# init graph
-for _ in range(nEdge):
-    st, end = map(int, f.readline().split())
-    graph[st].append((1, end))
+    # init graph
+    for _ in range(nEdge):
+        st, end = map(int, f.readline().split())
+        graph[st].append(end)
 
-print(graph)
+    distance = [-1] * (nCity + 1)
 
+    distance[start] = 0
 
-# def dijsktra(graph, start, k):
+    q = deque([start])
+    while q:
+        now = q.popleft()
+        for next_node in graph[now]:
+            if distance[next_node] == -1:
+                distance[next_node] = distance[now] + 1
+                q.append(next_node)
 
+    check = False
+    for i in range(1, nCity + 1):
+        if distance[i] == k:
+            print(i)
+            check = True
 
+    if check == False:
+        print(-1)
 
-
-
+    print('*----------*')
