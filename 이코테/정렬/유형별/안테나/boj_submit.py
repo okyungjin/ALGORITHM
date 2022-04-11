524aba36a54a673e454a0523ac90466d4fcc5904
okyungjin <okyungjin@gmail.com>
2022-04-11 12:29:48 +0900

    [이코테] 정렬 - 안테나 풀이

--- 이코테/정렬/유형별/안테나/boj_submit.py
+++ 이코테/정렬/유형별/안테나/boj_submit.py
@@ -6,31 +6,14 @@
 
 # https://www.acmicpc.net/problem/18310
 # [1차] 시간 초과
+# [2차] 정답
 
 import sys
 
 input = sys.stdin.readline
 
 n = int(input())
-houses = list(map(int, input().split()))
+data = list(map(int, input().split()))
+data.sort()
 
-def get_distances(houses, idx):
-    dist = houses[idx]
-    result = 0
-    for pos in houses:
-        result += abs(dist - pos)
-    return result
-
-
-houses.sort()
-min_val = 100000
-min_idx = 0
-idx = 0
-while idx >= 0 and idx < len(houses):
-    temp = get_distances(houses, idx)
-    if temp < min_val:
-        min_val = temp
-        min_idx = idx
-    idx += 1
-
-print(houses[min_idx])
+print(data[(n - 1) // 2])
--- 이코테/정렬/유형별/안테나/solution.py
+++ 이코테/정렬/유형별/안테나/solution.py
@@ -12,25 +12,7 @@ __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file
 f = open(os.path.join(__location__, 'input.txt'), 'r')
 
 n = int(f.readline())
-houses = list(map(int, f.readline().split()))
+data = list(map(int, f.readline().split()))
+data.sort()
 
-def get_distances(houses, idx):
-    dist = houses[idx]
-    result = 0
-    for pos in houses:
-        result += abs(dist - pos)
-    return result
-
-
-houses.sort()
-min_val = 100000
-min_idx = 0
-idx = 0
-while idx >= 0 and idx < len(houses):
-    temp = get_distances(houses, idx)
-    if temp < min_val:
-        min_val = temp
-        min_idx = idx
-    idx += 1
-
-print(houses[min_idx])
+print(data[(n - 1) // 2])
