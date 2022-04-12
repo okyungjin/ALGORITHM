94aeac7b63850259aebd5c705f0a0e44b831664b
okyungjin <okyungjin@gmail.com>
2022-04-12 22:24:29 +0900

    Update template.py

--- template.py
+++ template.py
@@ -4,7 +4,12 @@
 # 풀이 시간 m분
 # 시간 제한 s초
 
+# Normal
 import os
-
 __location__ = os.path.realpath(os.path.join(os.getcwd(), os.path.dirname(__file__)))
 f = open(os.path.join(__location__, 'input.txt'), 'r')
+input = f.readline
+
+# BOJ
+import sys
+input = sys.stdin.readline
