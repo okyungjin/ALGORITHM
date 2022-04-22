af830b415d9b9ab7b08f6488efe22990cd614c27
okyungjin <okyungjin@gmail.com>
2022-04-22 12:44:01 +0900

    [프로그래머스] DFS / BFS - 단어 변환 풀이

--- programmers/dfs & bfs/단어변환/solution.py
+++ programmers/dfs & bfs/단어변환/solution.py
@@ -1,6 +1,41 @@
+INF = 10e9
+answer = INF
+
 def solution(begin, target, words):
-    answer = 0
+    if target not in words:
+        return 0
+
+    n = len(words)
+    visited = [False] * n
+
+    dfs(begin, target, words, n, visited, 0)
+
     return answer
 
 
-solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])
+def diff_one_char(a, b):
+    diff = 0
+    for i in range(len(a)):
+        if a[i] != b[i]:
+            diff += 1
+    return diff == 1
+
+
+def dfs(start, target, words, n, visited, count):
+    global answer
+
+    if start == target:
+        if answer > count:
+            answer = count
+        return
+    
+    for i in range(n):
+        if diff_one_char(start, words[i]) and not visited[i]:
+            visited[i] = True
+            dfs(words[i], target, words, n, visited, count + 1)
+            visited[i] = False # back-tracking
+
+
+
+ans = solution('hit', 'cog', ['hot', 'dot', 'dog', 'lot', 'log', 'cog'])
+print(ans)
