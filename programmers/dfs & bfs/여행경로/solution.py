84b4f6550893b7d061ee42bb1a036d556a3d0095
okyungjin <okyungjin@gmail.com>
2022-04-25 22:36:36 +0900

    [프로그래머스] DFS / BFS - 여행 경로 풀이 (reference)

--- programmers/dfs & bfs/여행경로/solution.py
+++ programmers/dfs & bfs/여행경로/solution.py
@@ -11,18 +11,25 @@ def solution(tickets):
         routes.setdefault(start, [])
         heapq.heappush(routes[start], end)
 
-    go(len(tickets), routes, 'ICN')
+    go(len(tickets), routes, 'ICN', 0)
     return answer
 
 
-def go(n, routes, start):
-    if n + 1 == len(answer):
+def go(n, routes, start, used_tickets):
+    if n == used_tickets:
+        return
+
+    if start not in routes.keys() or len(routes[start]) == 0:
+        backstack = answer.pop()
+        go(n, routes, backstack, used_tickets - 1)
         return
     
     nxt = heapq.heappop(routes[start])
     answer.append(nxt)
-    go(n, routes, nxt)
+    go(n, routes, nxt, used_tickets + 1)
 
     
-# print(solution([['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]))
+print(solution([['ICN', 'JFK'], ['HND', 'IAD'], ['JFK', 'HND']]))
 print(solution([['ICN', 'SFO'], ['ICN', 'ATL'], ['SFO', 'ATL'], ['ATL', 'ICN'], ['ATL','SFO']]))
+
+# Backtrack 로직 추가가 필요함 
