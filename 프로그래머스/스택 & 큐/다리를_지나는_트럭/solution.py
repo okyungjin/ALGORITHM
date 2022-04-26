a816471286bac50f235fb6b2dc7334df32a9ba43
okyungjin <okyungjin@gmail.com>
2022-04-26 13:32:33 +0900

    [프로그래머스] 스택 / 큐 - 다리를 지나는 트럭 (WIP)

---
+++ 프로그래머스/스택 & 큐/다리를_지나는_트럭/solution.py
@@ -0,0 +1,21 @@
+from collections import deque
+import heapq
+
+
+def solution(bridge_length, weight, truck_weights):
+    trucks = deque(truck_weights)
+    
+    return trucks
+
+
+print(solution(2, 10, [7,4,5,6]))
+# print(solution(100, 100, [10]))
+# print(solution(100, 100, [10,10,10,10,10,10,10,10,10,10]))
+
+
+# bridge라는 큐를 만들어서 담아
+# 시간 계속 카운트하면서
+# truck_wiight에서 pop해서 bridge에 담아
+
+# tw이 비고 bridge도 빌때까지 반복해
+
