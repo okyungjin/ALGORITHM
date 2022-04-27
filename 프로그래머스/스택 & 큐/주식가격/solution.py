0f9a44b1a5d8aced1648864ddd55eec41a0a1ffb
okyungjin <okyungjin@gmail.com>
2022-04-27 12:45:21 +0900

    [프로그래머스] 스택 / 큐 - 주식가격 풀이

--- 프로그래머스/스택 & 큐/주식가격/solution.py
+++ 프로그래머스/스택 & 큐/주식가격/solution.py
@@ -7,20 +7,18 @@ def solution(prices):
 
     while q:
         cur = q.popleft()
-        answer.append(count(cur, q))
+        answer.append(count_time(cur, q))
 
     return answer
 
-def count(price, queue):
-    result = 0
+
+def count_time(price, queue):
+    time = 0
     for q in queue:
-        if q >= price:
-            result += 1
-    return result
+        time += 1
+        if price > q:
+            break
+    return time
 
 
 print(solution([1, 2, 3, 2, 3]))
-
-
-# [정확성 테스트] 테스트 1 제외 오답
-# [효율성 테스트] 오답 ..
