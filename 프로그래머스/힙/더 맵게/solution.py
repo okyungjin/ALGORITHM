d653f63c0f281818f34ff50dffd5af7f50226f32
okyungjin <okyungjin@gmail.com>
2022-04-28 09:32:04 +0900

    [프로그래머스] 힙 - 더 맵게 풀이

--- 프로그래머스/힙/더 맵게/solution.py
+++ 프로그래머스/힙/더 맵게/solution.py
@@ -1,19 +1,21 @@
 import heapq
 
 def solution(scoville, K):
-    mix_count = 0
-    while scoville:
-        mildest = heapq.heappop(scoville)
+    mix_count = 0 
+    heapq.heapify(scoville)
 
-        if mildest >= K:
+    while scoville:
+        if len(scoville) == 1 and scoville[0] < K:
+            return -1
+
+        if scoville[0] >= K:
             return mix_count
 
-        sec_mildest = heapq.heappop(scoville)
-        mixed = mix_scoville(mildest, sec_mildest)
+        mixed = mix_scoville(heapq.heappop(scoville), heapq.heappop(scoville))
         heapq.heappush(scoville, mixed)
         mix_count += 1
     
-    return -1
+    return mix_count
 
 
 def mix_scoville(mildest, sec_mildest):
@@ -21,6 +23,9 @@ def mix_scoville(mildest, sec_mildest):
 
 
 print(solution([1, 2, 3, 9, 10, 12], 7))
+print(solution([3, 9, 1, 2, 12, 10], 7))
+
+print(solution([1,2], 7))
 
 
 # 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
