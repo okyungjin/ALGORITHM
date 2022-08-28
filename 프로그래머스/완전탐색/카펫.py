8cd7df11faf838ba7e47548d2133ded0e8c2426b
okyungjin <okyungjin@gmail.com>
2022-08-29 00:07:38 +0900

    [프로그래머스] 카펫

--- 프로그래머스/완전탐색/카펫.py
+++ 프로그래머스/완전탐색/카펫.py
@@ -4,16 +4,36 @@
 from math import sqrt, ceil
 
 def solution(brown, yellow):
-    total = brown + yellow
+        yellow_width = yellow
+        yellow_height = 1
 
-    for height in range(3, ceil(sqrt(total)) + 1):
-        if total % height != 0: continue
-        width = total // height
-        if height > width: continue
+        while yellow_width >= yellow_height:
+            # 테두리 개수 비교
+            if 2 * (yellow_width + yellow_height) + 4 == brown:
+                return [yellow_width + 2, yellow_height + 2]
 
-        if 2 * (width + height) - 4 + (width - 2) * (height - 2) == total and (width - 2) * (height - 2) == yellow:
-            return [width, height]
-        
+            yellow_height += 1
+            while yellow % yellow_height != 0:
+                yellow_height += 1
+            yellow_width = yellow // yellow_height
+
+
+# 22.08.29
+class Solve1:
+    def solution(brown, yellow):
+        total = brown + yellow
+
+        for height in range(3, ceil(sqrt(total)) + 1):
+            if total % height != 0: continue
+            width = total // height
+            if height > width: continue
+
+            if 2 * (width + height) - 4 + (width - 2) * (height - 2) == total and (width - 2) * (height - 2) == yellow:
+                return [width, height]
+
+
+
+
     
 
 print(solution(10, 2))
