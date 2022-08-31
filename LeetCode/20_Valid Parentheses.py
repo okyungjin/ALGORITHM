2291f87ee3474e53e3a040a1a672c4187567d52f
okyungjin <okyungjin@gmail.com>
2022-09-01 00:24:58 +0900

    [LeetCode] 20. Valid Parentheses

--- LeetCode/20_Valid Parentheses.py
+++ LeetCode/20_Valid Parentheses.py
@@ -4,21 +4,22 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        bracket_pair = {'}':'{', ')':'(', ']':'['}
+        bracket_pair = {')':'(', '}':'{', ']':'['}
 
         for bracket in s:
             if bracket in bracket_pair.values():
                 stack.append(bracket)
+                continue
+
+            if stack and bracket_pair[bracket] == stack[-1]:
+                stack.pop()
             else:
-                if stack and bracket_pair[bracket] == stack[-1] :  
-                    stack.pop()
-                else: 
-                    return False
-        
-        if stack: return False
-        return True
+                return False 
+            
+        return False if stack else True
 
 
 solution = Solution()
-print(solution.isValid('()'))
-# print(solution.isValid('()[]{}'))
+print(solution.isValid('()')) # True
+print(solution.isValid('()[]{}')) # True
+print(solution.isValid('(]')) # False
